40625d29870c8bd6badb2cc9fac52dd6db97bb0b
lokmen abid <lokmenabid@gmail.com>
2021-03-16 13:40:49 +0100

    init

---
+++ 1proj.js
@@ -0,0 +1,54 @@
+function book(title,author,genre,description){
+    return {
+        title: title,
+        author: author,
+        genre : genre,
+        description:description
+    };
+}
+
+var book1 = book ('Romeo & Juliet' ,'William Shakespeare','Romance ','Romeo and Juliet is a tragedy written by William Shakespeare early in his career about two young star-crossed lovers whose deaths ultimately reconcile their feuding families. It was among Shakespeares most popular plays during his lifetime and, along with Hamlet, is one of his most frequently performed plays.') ;
+var book2 = book ('Jane Eyre' ,'Charlotte Brontë','Romance ','The novel follows the story of Jane, a seemingly plain and simple girl as she battles through lifes struggles. Jane has many obstacles in her life - her cruel and abusive Aunt Reed, the grim conditions at Lowood school, her love for Rochester and Rochesters marriage to Bertha.') ;
+var book3 = book ('The Princess Bride' ,'William Goldman','Romance ','The Princess Bride, it tells the story of a farmhand named Westley, accompanied by companions befriended along the way, who must rescue his true love Princess Buttercup from the odious Prince Humperdinck.') ;
+var book4 = book ('The Little Prince' ,'Antoine de Saint-Exupéry','Romance ','The story follows a young prince who visits various planets in space, including Earth, and addresses themes of loneliness, friendship, love, and loss. Despite its style as a childrens book, The Little Prince makes observations about life and human nature.') ;
+var book5 = book ('The Truth About Forever' ,'Sarah Dessen','Romance ',"Macy is left reeling by her beloved father's sudden death while they were out running together. Macy's mother is unable to grieve, while Macy's sister puts her energy into renovating their father's old beach house.") ;
+var book6 = book ("Pride and Prejudice","Jane Austen","Classics","Pride and Prejudice follows the turbulent relationship between Elizabeth Bennet, the daughter of a country gentleman, and Fitzwilliam Darcy, a rich aristocratic landowner. They must overcome the titular sins of pride and prejudice in order to fall in love and marry.");
+var book7 = book ("The Great Gatsby","F. Scott Fitzgerald","Classics","The story is told by Nick Carraway, a man who moves to Long Island, New York, from the Midwest. Nick is not rich, but he lives in a rich area that has two towns called East Egg and West Egg. The old rich live in East Egg while the new rich live in West Egg. Nick lives in a small house in West Egg. Nick's next-door neighbor is Jay Gatsby. Jay is in love with Nick's cousin Daisy. However, Daisy is married to a man named Tom. The novel is about Jay and his hope that he can steal Daisy from Tom.");
+var book8 = book ("Brave New World","Aldous Huxley","Classics","The novel examines a futuristic society, called the World State, that revolves around science and efficiency. In this society, emotions and individuality are conditioned out of children at a young age, and there are no lasting relationships because “every one belongs to every one else”");
+var book9 = book ("The Call of the Wild","Jack London","Classics","The Call of the Wild is a short adventure novel by Jack London, published in 1903 and set in Yukon, Canada, during the 1890s Klondike Gold Rush, when strong sled dogs were in high demand. The central character of the novel is a dog named Buck. ... The book's great popularity and success made a reputation for London."); 
+var book10 = book ("Moby-Dick","Herman Melville","Classics","The novel Moby Dick by Herman Melville is an epic tale of the voyage of the whaling ship the Pequod and its captain, Ahab, who relentlessly pursues the great Sperm Whale (the title character) during a journey around the world.");
+var book11 = book ("Grant","Ron Chernow","Historical","Pulitzer Prize winner Ron Chernow returns with a sweeping and dramatic portrait of one of our most compelling generals and presidents, Ulysses S. Grant. With lucidity, breadth, and meticulousness, Chernow finds the threads that bind Grant's disparate stories together, shedding new light on the man whom Walt Whitman described as nothing heroic...and yet the greatest hero.");
+var book12 = book ("The History of the Ancient World"," Susan Wise Bauer","Historical","A lively and engaging narrative history showing the common threads in the cultures that gave birth to our own. Dozens of maps provide a clear geography of great events, while timelines give the reader an ongoing sense of the passage of years and cultural interconnection. Literature, epic traditions, private letters and accounts connect kings and leaders with the lives of those they ruled.");
+var book13 = book ("Democracy: A Life"," Paul Cartledge","Historical","Ancient Greece first coined the concept of democracy, yet almost every major ancient Greek thinker-from Plato and Aristotle onwards was ambivalent towards or even hostile to democracy in any form. The explanation for this is quite simple: the elite perceived majority power as tantamount to a dictatorship of the proletariat. Cartledge sheds light on the variety of democratic practices in the classical world as well as on their similarities to and dissimilarities from modern democratic forms, from the American and French revolutions to contemporary political thought.");
+var book14 = book ("Churchill: Walking with Destiny","Andrew Roberts","Historical","We think of Churchill as a hero who saved civilization from the evils of Nazism and warned of the grave crimes of Soviet communism, but Roberts's masterwork reveals that he has as much to teach us about the challenges leaders face today - and the fundamental values of courage, tenacity, leadership and moral conviction.");
+var book15 = book ("Appeasement"," Tim Bouverie","Historical","Appeasement is a groundbreaking history of the disastrous years of indecision, failed diplomacy and parliamentary infighting that enabled Hitler's domination of Europe. Drawing on deep archival research and sources not previously seen by historians, Tim Bouverie has created an unforgettable portrait of the ministers, aristocrats, and amateur diplomats who, through their actions and inaction, shaped their country's policy and determined the fate of Europe.");
+var book16 = book ("Scoop","Evelyn Waugh","Comedy","Lord Copper, newspaper magnate and proprietor of the Daily Beast, has always prided himself on his intuitive flair for spotting ace reporters. That is not to say he has not made the odd blunder, however, and may in a moment of weakness make another. Acting on a dinner-party tip from Mrs Algernon Smith, he feels convinced that he has hit on just the chap to cover a promising little war in the African Republic of Ishmaelia. One of Evelyn Waugh's most exuberant comedies, Scoop is a brilliantly irreverent satire of Fleet Street and its hectic pursuit of hot news");
+var book17 = book ("Grown Ups","Marian Keyes","Comedy","Johnny Casey, his two brothers Ed and Liam, their beautiful, talented wives and all their kids spend a lot of time together - birthday parties, anniversary celebrations, weekends away. And they're a happy family. Johnny's wife, Jessie - who has the most money - insists on it.");
+var book18 = book ("Lucky Jim","Kingsley Amis","Comedy","Regarded by many as the finest, and funniest, comic novel of the twentieth century, Lucky Jim remains as trenchant, withering, and eloquently misanthropic as when it first scandalized readers back in 1954. This is the story of Jim Dixon, a hapless lecturer in medieval history at a provincial university who knows better than most that “there was no end to the ways in which nice things are nicer than nasty ones.” Kingsley Amis’s scabrous debut leads the reader through a gallery of emphatically English bores, cranks, frauds, and neurotics with whom Dixon must contend in one way or another in order to hold on to his cushy academic perch and win the girl of his fancy.");
+var book19 = book ("White teeth","Zadie Smith","Comedy","At the center of this invigorating novel are two unlikely friends, Archie Jones and Samad Iqbal. Hapless veterans of World War II, Archie and Samad and their families become agents of England’s irrevocable transformation. A second marriage to Clara Bowden, a beautiful, albeit tooth-challenged, Jamaican half his age, quite literally gives Archie a second lease on life, and produces Irie, a knowing child whose personality doesn’t quite match her name (Jamaican for “no problem”). ");
+var book20 = book ("Reasons to Be Cheerful","Nina Stibbe","Comedy","When people in the village heard I was about to start working in the city they tried to unsettle me with tales of woe. The sun, blotted out by the tall buildings, couldn't shine and the rain was poisoned by the toxic fumes that poured from the sock factories. My skin would be covered in pimples from the hell of it all"); 
+
+
+var Books =[book1,book2,book3,book4,book5,book6,book7,book8,book9,book10,book11,book12,book13,book14,book15,book16,book17,book18,book19,book20]; 
+
+function READ (){
+    var $Bok=$('#Bok') ;
+    var bookTitle = $('#srch').val()
+    $Bok.html('') ; 
+    var index = 0 ; 
+    while(index<Books.length){
+        var boek=Books[index] ;
+        console.log(boek, bookTitle)
+        if(boek.title.toLowerCase()===bookTitle.toLowerCase()){
+            $boek = $('<div><b>' + boek.title+' </b><br> '+boek.author+' <br> '+boek.genre+' <br><br> '+boek.description + '</div>')
+            $boek.appendTo($Bok); 
+        }
+        
+            index +=1  ; 
+    }
+}
+
+// $(document).ready(function(){
+//     // READ();
+// }); 
+
